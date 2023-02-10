a48ed517eeba35b5c6b9e9905c85fcc3767f7bc8
Aziz Rakhim <azizbekrahimjonov80@gmail.com>
2023-02-10 15:00:12 +0500

    feat: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useCSVReader, usePapaParse, useCSVDownloader } from "react-papaparse";
+import "./App.css";
 
 function App() {
+  const { CSVReader } = useCSVReader();
+  const { jsonToCSV } = usePapaParse();
+  const { CSVDownloader, Type } = useCSVDownloader();
+
+  const [tableData, setTableData] = useState<any>([]);
+  const [header, setHeader] = useState<any>([]);
+
+  const saveFile = () => {
+    let csv = jsonToCSV(tableData);
+
+    console.log(csv);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <CSVReader
+        onUploadAccepted={(results: any) => {
+          setTableData(results.data.filter((data: any, i: number) => i !== 0));
+          setHeader(results.data[0]);
+        }}
+      >
+        {({ getRootProps, acceptedFile }: any) => (
+          <>
+            <div className="btn-container">
+              <button type="button" {...getRootProps()} className="button">
+                Browse file
+              </button>
+              <div>{acceptedFile && acceptedFile.name}</div>
+            </div>
+            <hr />
+          </>
+        )}
+      </CSVReader>
+      <div className="header-container">
+        <h2>User Table</h2>
+        <div className="btn-container">
+          <button onClick={saveFile} className="convert">
+            JSON to CSV
+          </button>
+          <CSVDownloader
+            type={Type.Button}
+            filename={"filename"}
+            bom={true}
+            data={tableData}
+            className="download"
+          >
+            Download
+          </CSVDownloader>
+        </div>
+      </div>
+      {tableData.length ? (
+        <table>
+          <thead>
+            <tr>
+              {header.map((header: any, index: number) => (
+                <th key={index}>{header}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {tableData.map((data: any, index: number) => (
+              <tr key={index}>
+                {data.map((datum: any, i: number) => (
+                  <td key={i}>
+                    <input
+                      type="text"
+                      value={datum}
+                      className="input"
+                      onChange={(e: any) => {
+                        const tempData = tableData.map(
+                          (item: any, idx: number) => {
+                            if (idx === index) {
+                              item.map((el: any, _index: number) => {
+                                if (_index === i) {
+                                  return e.target.value;
+                                }
+                                return el;
+                              });
+                            }
+                            return item;
+                          }
+                        );
+                        setTableData(tempData);
+                      }}
+                    />
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <div className="no-data">No data</div>
+      )}
     </div>
   );
 }
